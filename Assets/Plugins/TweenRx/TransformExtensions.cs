08f989a18611299ea089c66750264ac0be3afd73
Fumo Yoshida <fumobox@gmail.com>
2018-09-23 23:28:14 +0900

    Revert "Fix ambiguous reference for .Net 4.x"
    This reverts commit 00e47824a72b73d073d25ed864d2bb79a5a8bd6d.

--- Assets/Plugins/TweenRx/TransformExtensions.cs
+++ Assets/Plugins/TweenRx/TransformExtensions.cs
@@ -6,12 +6,12 @@ namespace TweenRx
 {
     public static class TransformExtensions
     {
-        public static IDisposable SubscribeToLocalPosition(this UniRx.IObservable<Vector3> source, Transform transform)
+        public static IDisposable SubscribeToLocalPosition(this IObservable<Vector3> source, Transform transform)
         {
             return source.SubscribeWithState(transform, (x, t) => t.localPosition = x);
         }
 
-        public static UniRx.IObservable<Vector3> DoToLocalPosition(this UniRx.IObservable<Vector3> source, Transform transform)
+        public static IObservable<Vector3> DoToLocalPosition(this IObservable<Vector3> source, Transform transform)
         {
             return source.Do(x => transform.localPosition = x);
         }
